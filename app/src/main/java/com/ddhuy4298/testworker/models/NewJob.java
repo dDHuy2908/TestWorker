3b89dcc6b9c809583ea6411a4a33bae72df06445
ddHuy_bobe <dDHuy4298>
2020-06-09 12:54:46 +0700

    update

---
+++ app/src/main/java/com/ddhuy4298/testworker/models/NewJob.java
@@ -0,0 +1,59 @@
+package com.ddhuy4298.testworker.models;
+
+public class NewJob {
+
+    private String address;
+    private String date;
+    private String time;
+    private String userId;
+    private String job;
+    private String requestId;
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
